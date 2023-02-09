1c759ac1623de24e314968a4455598283a81d12c
Dominik Orincsai <o.dominik2107@gmail.com>
2023-02-09 22:54:21 +0100

    feat(*): removed usestate from app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from 'react';
 import GlobalStyle from './globalStyle';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <GlobalStyle />
