c6709e537d168c568de87b8f017c9a88ef88d3e9
Dominik Orincsai <o.dominik2107@gmail.com>
2023-02-10 01:32:22 +0100

    feat(game): game field wip

--- src/pages/PlayGround/PlayGround.jsx
+++ src/pages/PlayGround/PlayGround.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import {
+  CellInput,
+  GameTable,
+  GameTableBody,
+  GameTableData,
+  GameTableRow,
+  PageContainer,
+} from './styles';
+
+const cellNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 const PlayGround = () => {
   return (
-    <div>
-      <h2>Hello</h2>
-    </div>
+    <PageContainer>
+      <GameTable>
+        <GameTableBody>
+          {cellNumbers.map((row, rowIndex) => {
+            return (
+              <GameTableRow key={rowIndex}>
+                {cellNumbers.map((column, columnIndex) => {
+                  return (
+                    <GameTableData key={rowIndex + columnIndex}>
+                      <CellInput />
+                    </GameTableData>
+                  );
+                })}
+              </GameTableRow>
+            );
+          })}
+        </GameTableBody>
+      </GameTable>
+    </PageContainer>
   );
 };
 
---
+++ src/pages/PlayGround/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const PageContainer = styled.div`
+  display: flex;
+`;
+
+export const GameTable = styled.table``;
+
+export const GameTableBody = styled.tbody``;
+
+export const GameTableRow = styled.tr``;
+
+export const GameTableData = styled.td``;
+
+export const CellInput = styled.input`
+  width: 50px;
+  height: 50px;
+  font-size: 20px;
+  text-align: center;
+  padding: 0px;
+  border: 1px solid black;
+`;
