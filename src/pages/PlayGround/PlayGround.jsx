ba76ead3eebc18138c915b1f7bc0777d6e0cf560
Dominik Orincsai <o.dominik2107@gmail.com>
2023-02-10 02:40:01 +0100

    feat(game): added the backtracking algorithm to solve the puzzle + prevent letters

--- src/pages/PlayGround/PlayGround.jsx
+++ src/pages/PlayGround/PlayGround.jsx
@@ -6,26 +6,99 @@ import {
   GameTableData,
   GameTableRow,
   PageContainer,
+  SolveButton,
 } from './styles';
 
 const PlayGround = () => {
   const [grid, setGrid] = useState(
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    Array(9)
+      .fill()
+      .map(() => Array(9).fill(0)),
   );
+
+  const handleChange = (event, rowIndex, columnIndex) => {
+    const updatedGrid = [...grid];
+    updatedGrid[rowIndex][columnIndex] = event.target.value;
+    setGrid(updatedGrid);
+  };
+
+  const handleKeyPress = (event) => {
+    const charCode = event.charCode || event.which;
+
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      event.preventDefault();
+    }
+  };
+
+  const solve = () => {
+    const solution = solveSudoku(grid);
+    setGrid(solution);
+  };
+
+  const solveSudoku = (grid) => {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (grid[i][j] === 0) {
+          for (let k = 1; k <= 9; k++) {
+            if (isValid(grid, i, j, k)) {
+              grid[i][j] = k;
+
+              if (solveSudoku(grid)) {
+                return grid;
+              } else {
+                grid[i][j] = 0;
+              }
+            }
+          }
+          return false;
+        }
+      }
+    }
+    return grid;
+  };
+
+  const isValid = (grid, row, column, number) => {
+    for (let i = 0; i < 9; i++) {
+      if (grid[row][i] === number || grid[i][column] === number) {
+        return false;
+      }
+    }
+
+    const rowStart = Math.floor(row / 3) * 3;
+    const columnStart = Math.floor(column / 3) * 3;
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (grid[rowStart + i][columnStart + j] === number) {
+          return false;
+        }
+      }
+    }
+    return true;
+  };
 
   return (
     <PageContainer>
       <GameTable>
-        <GameTableBody></GameTableBody>
+        <GameTableBody>
+          {grid.map((row, rowIndex) => (
+            <GameTableRow key={rowIndex}>
+              {row.map((column, columnIndex) => (
+                <GameTableData key={columnIndex}>
+                  <CellInput
+                    type='text'
+                    maxLength='1'
+                    value={grid[rowIndex][columnIndex] || ''}
+                    onChange={(e) => handleChange(e, rowIndex, columnIndex)}
+                    onKeyPress={handleKeyPress}
+                  />
+                </GameTableData>
+              ))}
+            </GameTableRow>
+          ))}
+        </GameTableBody>
       </GameTable>
+      <SolveButton onClick={solve}>Solve</SolveButton>
     </PageContainer>
   );
 };
--- src/pages/PlayGround/styles.js
+++ src/pages/PlayGround/styles.js
@@ -2,12 +2,14 @@ import styled from 'styled-components';
 
 export const PageContainer = styled.div`
   display: flex;
+  flex-direction: column;
 `;
 
 export const GameTable = styled.table`
   border-collapse: collapse;
   border: 3px solid #51e2f5;
   background-color: #51e2f5;
+  margin: 0 0 15px 0;
 `;
 
 export const GameTableBody = styled.tbody``;
@@ -26,3 +28,5 @@ export const CellInput = styled.input`
   padding: 0px;
   border: 1px solid black;
 `;
+
+export const SolveButton = styled.button``;
