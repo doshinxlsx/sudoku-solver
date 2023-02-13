f5b1ad359284cf1f5d94328baee0d6d57058263b
Dominik Orincsai <o.dominik2107@gmail.com>
2023-02-13 13:59:43 +0100

    feat(*): removed jest

--- src/pages/LandingPage/__tests__/LandingPage.test.js
+++ src/pages/LandingPage/__tests__/LandingPage.test.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import LandingPage from '../LandingPage';
-import { useLandingPage } from '../hooks';
+import { render, screen, fireEvent } from '@testing-library/react';
+import LandingPage from './LandingPage';
 
-jest.mock('../hooks', () => ({
-  mockHandleFakeApiCall: jest.fn(),
-  mockHandleNavigate: jest.fn(),
-}));
+describe('LandingPage', () => {
+  it('displays title and description', () => {
+    render(<LandingPage />);
 
-describe('LandingPage', () => {});
+    expect(screen.getByTestId('title')).toHaveTextContent('Sudoku solver');
+    expect(screen.getByTestId('description')).toHaveTextContent(
+      'Here, you can enter a sudoku puzzle and have it solved for you. If the puzzle is unsolvable, we will let you know.',
+    );
+  });
+
+  it('subscribes to email list', async () => {
+    const fakeApiCall = jest.fn();
+    jest.mock('./hooks', () => {
+      return () => ({
+        handleFakeApiCall: fakeApiCall,
+        handleNavigate: jest.fn(),
+      });
+    });
+
+    render(<LandingPage />);
+
+    const emailInput = screen.getByTestId('email-input');
+    const emailSubmitButton = screen.getByTestId('email-submit-button');
+    const email = 'test@example.com';
+
+    fireEvent.change(emailInput, { target: { value: email } });
+    fireEvent.click(emailSubmitButton);
+
+    expect(fakeApiCall).toHaveBeenCalled();
+  });
+
+  it('navigates to game page', () => {
+    const handleNavigate = jest.fn();
+    jest.mock('./hooks', () => {
+      return () => ({
+        handleFakeApiCall: jest.fn(),
+        handleNavigate,
+      });
+    });
+
+    render(<LandingPage />);
+
+    const playButton = screen.getByTestId('play-button');
+    fireEvent.click(playButton);
+
+    expect(handleNavigate).toHaveBeenCalled();
+  });
+});
